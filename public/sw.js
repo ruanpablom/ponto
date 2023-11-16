274ceb7210826542f04e8b5b0fe25c56aedbfe3a
Ruan Medeiros <pm.ruan@gmail.com>
2023-11-16 17:22:45 -0300

    refactor: background color manifest

--- public/sw.js
+++ public/sw.js
@@ -2,14 +2,14 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
+precacheAndRoute([...self.__WB_MANIFEST, { url: '/ponto', revision: null }]);
+
 registerRoute(
   '/ponto',
   new NetworkFirst({
     cacheName: 'root-cache',
   }),
 );
-
-precacheAndRoute([...self.__WB_MANIFEST, { url: 'ponto', revision: null }]);
 
 if (Notification.permission === 'default') {
   Notification.requestPermission();
