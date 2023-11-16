0779c54b124e5bdb4f04220ccab39c78a8664c9c
Ruan Medeiros <pm.ruan@gmail.com>
2023-11-16 17:10:39 -0300

    refactor: service worker

--- public/sw.js
+++ public/sw.js
@@ -2,13 +2,14 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
-precacheAndRoute(self.__WB_MANIFEST);
 registerRoute(
-  '/',
+  '/ponto',
   new NetworkFirst({
     cacheName: 'root-cache',
   }),
 );
+
+precacheAndRoute(self.__WB_MANIFEST);
 
 if (Notification.permission === 'default') {
   Notification.requestPermission();
