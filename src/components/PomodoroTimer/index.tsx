6dd46d25de405f4b83f0bf9f3433a618c0baf8fa
Ruan Medeiros <pm.ruan@gmail.com>
2023-11-16 03:19:01 -0300

    feat: notification

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -18,12 +18,12 @@ export function PomodoroTimer(): JSX.Element {
   const sendNotification = useCallback(() => {
     if (Notification.permission === 'granted') {
       // eslint-disable-next-line no-new
-      console.info(navigator.serviceWorker);
+      console.info(navigator.serviceWorker.ready);
       navigator.serviceWorker.ready.then(registration => {
         console.info('Notification send');
         registration.showNotification('Ponto', {
           body: 'Adicione uma nova atividade e reinicie o timer.',
-          icon: '/favicon-32x32',
+          // icon: '/favicon-32x32',
         });
       });
     }
