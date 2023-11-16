9aaff7dad5a3bb75a7a84b77a419d0a0bd2c134e
Ruan Medeiros <pm.ruan@gmail.com>
2023-11-16 14:17:21 -0300

    refactor: service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { registerSW } from 'virtual:pwa-register';
 
 import App from './App';
 import './index.css';
-
-const intervalMS = 60 * 60 * 1000;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const updateSW = registerSW({
-  onRegistered(r) {
-    r &&
-      setInterval(() => {
-        console.info('SW Registered: ', r);
-        r.update();
-      }, intervalMS);
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
