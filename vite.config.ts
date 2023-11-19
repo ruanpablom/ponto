fe382c03ac31b9e0f2bf8d9fd8109087628c27d1
Ruan Medeiros <pm.ruan@gmail.com>
2023-11-18 22:25:41 -0300

    refactor: sw

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 const manifestForPlugIn: Partial<VitePWAOptions> = {
-  // includeAssets: [
-  //   'favicon.ico',
-  //   'apple-touch-icon.png',
-  //   'masked-icon.svg',
-  //   '**/*',
-  // ],
-  strategies: 'injectManifest',
+  // strategies: 'injectManifest',
   manifest: {
     name: 'Ponto RPMDev',
     short_name: 'Ponto',
